8bde5249ecf6d9ca576fad9eaa57c5dece2bb02d
je-boska <joneirikboska@gmail.com>
2023-01-02 19:55:42 +0100

    Add meta description

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -1,12 +1,14 @@
 import Head from 'next/head';
-import React from 'react';
 
 export default function Meta({ title }) {
   return (
     <>
       <Head>
         <title>{title}</title>
-        <meta name='description' content='Transfer' />
+        <meta
+          name='description'
+          content='Transfer are an interdisciplinary research group that examines alternative means of contact with others. '
+        />
         <link rel='icon' href='/favicon.ico' />
       </Head>
     </>
