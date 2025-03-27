b69795e16875c327416eaff0688b9c8db4eac8ab
je-boska <joneirikboska@gmail.com>
2025-03-27 16:19:55 +0100

    Add url to permitted image sources

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
-const { i18n } = require("./next-i18next.config");
+const { i18n } = require('./next-i18next.config');
 
 module.exports = {
   reactStrictMode: true,
   i18n,
   images: {
-    domains: ["images.ctfassets.net", "videos.ctfassets.net"],
+    domains: [
+      'images.ctfassets.net',
+      'videos.ctfassets.net',
+      'downloads.ctfassets.net',
+    ],
   },
 };
